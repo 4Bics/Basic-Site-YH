53d8c2c1cb45cbe9e39c1bb6a9330de36d91b6f7
Cameron Maclean <cm.maclean@yahoo.com>
2021-03-03 16:23:24 +0000

    Added some file structure - containers/components with basic files within

---
+++ src/Containers/MainContainer.js
@@ -0,0 +1,14 @@
+import {Container, Header} from 'semantic-ui-react';
+import Footer from '../Components/Footer';
+
+function MainContainer () {
+
+  return (
+    <Container>
+      <Header>This is the header for the main page container</Header>
+      <Footer/>
+    </Container>
+  );
+};
+
+export default MainContainer;
